7ac35636006bcac91d05eb07b1648417992f0765
JanHenst <42889823+JanHenst@users.noreply.github.com>
2019-04-08 14:50:35 +0200

    Add files via upload

---
+++ dbVars.php
@@ -0,0 +1,11 @@
+<?php
+$severname = "127.0.0.1";
+$uid = "c3676BenDom";
+$pwd = "bFMhrCpMDt2tL_";
+$database = "c3676ajax3";
+
+// $severname = "localhost";
+// $uid = "root";
+// $pwd = "";
+// $database = "datatest";
+?>
